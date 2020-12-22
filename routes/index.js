e8f6eb2243d448cff057befebc33b60176f96959
ladral <tuor_philipp@hotmail.com>
2020-12-22 16:05:10 +0100

    first implementation of an automatically generated navigation bar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const navigation = require('../views/partials/navigation.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { navItems:navigation.items, title: 'Express'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const navigation = require('../views/partials/navigation.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { navItems:navigation.items, title: 'Users', name: "world"});
 });
+
 
 module.exports = router;
